25db81ded89b0f5a07237cc13839ef2a5aa76846
icyeyeball <yeahhuman@gmail.com>
2019-08-27 19:18:57 +0800

    google search finished

--- word_crawler/google.py
+++ word_crawler/google.py
@@ -8,8 +8,6 @@ import requests
 import sys
 from bs4 import BeautifulSoup
 import re
-import time
-from langconv import *
 
 # Google 搜尋 URL
 google_url = 'https://www.google.com.tw/search?num=20&lr=lang_zh-TW&q='
@@ -35,8 +33,8 @@ if r.status_code == requests.codes.ok:
     #print (sp[sp.find('http://')-23:sp.find('http://')-18])
     items = soup.findAll("div", {"class": sp[sp.find('http://')-23:sp.find('http://')-18]})
     #items = soup.findAll("div", {"class": sp[sp.find('https://')-23:sp.find('https://')-18]})
-i = 1   
-with open('google.txt', 'w', encoding = 'utf-8') as f:
+
+with open('google.txt', 'w', encoding = 'utf-8') as f2:
     for item in items:
         # title
         #news_title = item.find("h3", {"class": "r"}).find("a").text
@@ -60,13 +58,11 @@ with open('google.txt', 'w', encoding = 'utf-8') as f:
                 stories = str(soup2.find_all('p', class_=""))
                 stories = cop.sub('', stories)
                 print (stories)
-                f.write(stories)
-                f.write('\n')
+                f2.write(stories)
+                f2.write('\n')
                 #f2.write("--------------------------i = " + str(i))
                 #f2.write('\n')
                 #f2.write("url = " + str(url))
                 #f2.write('\n')
         #f2.write("=================================================")
-        time.sleep(2)
-        f.write('\n')
-        i += 1
+        f2.write('\n')
